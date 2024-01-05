8843d8562595b88f2801f2e53819e5992d99363f
Jaime Medrano <jaime08medrano@gmail.com>
2024-01-05 16:20:38 -0600

    todo terminado

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,20 +1,31 @@
-import { type Todo as TodoType } from "../types"
+import { TodoId, type Todo as TodoType } from "../types"
 
-type Props = TodoType
+interface Props extends TodoType {
+    onToggleCompletedTodo: ({id, completed}: Pick<TodoType, 'id' | 'completed'>) => void
+    onRemoveTodo: ({id}:TodoId) => void
+}
 
-export const Todo: React.FC<Props> = ({ id, title, completed }) => {
+export const Todo: React.FC<Props> = ({ id, title, completed, onRemoveTodo, onToggleCompletedTodo }) => {
+    const handleChangeCheckbox = (event: React.ChangeEvent<HTMLInputElement>) => {
+        onToggleCompletedTodo({
+            id,
+            completed: event.target.checked
+        })
+    }
     return (
         <div>
             <input
                 className="toggle"
                 checked={completed}
                 type="checkbox" 
-                onChange={() => {}}
+                onChange={handleChangeCheckbox}
             />
             <label>{title}</label>
             <button 
                 className="destroy" 
-                onClick={() => {}} 
+                onClick={() => {
+                    onRemoveTodo({id})
+                }} 
             />   
         </div>
     )
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,11 +1,13 @@
-import { type ListOfTodos } from "../types"
+import { TodoId, type ListOfTodos, type Todo as TodoType } from "../types"
 import { Todo } from "./Todo"
 
 interface Props {
     todos: ListOfTodos
+    onToggleCompletedTodo: ({id, completed}: Pick<TodoType, 'id' | 'completed'>) => void
+    onRemoveTodo: ({id}: TodoId) => void
 }
 
-export const Todos: React.FC<Props> = ({ todos }) => {
+export const Todos: React.FC<Props> = ({ todos, onRemoveTodo, onToggleCompletedTodo }) => {
     return (
         <ul className="todo-list">
         {todos.map(todo => (
@@ -18,6 +20,8 @@ export const Todos: React.FC<Props> = ({ todos }) => {
                     id={todo.id}
                     title={todo.title}
                     completed={todo.completed}
+                    onToggleCompletedTodo={onToggleCompletedTodo}
+                    onRemoveTodo={onRemoveTodo}
                 />
             </li>
         ))}
