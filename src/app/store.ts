2913058aa6d2790cddac6f83b7d39502b224ef62
Jaime Medrano <jaime08medrano@gmail.com>
2024-02-05 12:04:47 -0600

    update redux

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from '../features/counter/counterSlice'
 import leagueFormSlice from '../features/leagues/leagueFormSlice'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     leagueForm: leagueFormSlice,
   },
 })
